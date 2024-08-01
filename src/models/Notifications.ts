4f72170a7de218bbf9cfcde359b710d151822079
Luu Tuan Hoang <hoanglt0104@gmail.com>
2024-08-02 03:33:05 +0700

    Decorator Notification implementation

---
+++ src/models/Notifications.ts
@@ -0,0 +1,62 @@
+interface CustomNotification {
+    send(message: string): void;
+}
+
+class BasicNotification implements CustomNotification {
+    send(message: string): void {
+        console.log(`Sending notification: ${message}`);
+    }
+}
+
+class SMSNotificationDecorator implements CustomNotification {
+    private wrapped: CustomNotification;
+
+    constructor(notification: CustomNotification) {
+        this.wrapped = notification;
+    }
+
+    send(message: string): void {
+        this.wrapped.send(message);
+        this.sendSMS(message);
+    }
+
+    private sendSMS(message: string): void {
+        console.log(`Sending SMS: ${message}`);
+    }
+}
+
+class EmailNotificationDecorator implements CustomNotification {
+    private wrapped: CustomNotification;
+
+    constructor(notification: CustomNotification) {
+        this.wrapped = notification;
+    }
+
+    send(message: string): void {
+        this.wrapped.send(message);
+        this.sendEmail(message);
+    }
+
+    private sendEmail(message: string): void {
+        console.log(`Sending Email: ${message}`);
+    }
+}
+
+class PushNotificationDecorator implements CustomNotification {
+    private wrapped: CustomNotification;
+
+    constructor(notification: CustomNotification) {
+        this.wrapped = notification;
+    }
+
+    send(message: string): void {
+        this.wrapped.send(message);
+        this.sendPush(message);
+    }
+
+    private sendPush(message: string): void {
+        console.log(`Sending Push Notification: ${message}`);
+    }
+}
+
+export { CustomNotification, BasicNotification, SMSNotificationDecorator, EmailNotificationDecorator, PushNotificationDecorator };
