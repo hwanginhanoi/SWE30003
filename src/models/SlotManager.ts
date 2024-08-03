dc75fdd5f2153e5a4b3a357215ed9cc25c3c382b
Thanh Duong <dou.peter17@gmail.com>
2024-08-03 22:32:31 +0700

    update

--- src/models/SlotManager.ts
+++ src/models/SlotManager.ts
@@ -2,21 +2,13 @@ import {PrismaClient, SlotStatus, SlotType} from "@prisma/client";
 import ParkingSlot from "./ParkingSlot";
 
 const prisma = new PrismaClient();
-class SlotMan
 
-    async deleteSlot()
-    {
+class ParkingLot {
 
-    }
+// Add more methods for managing slots if needed
 
-    public getName(): string {
-        return this.name;
-    }
+    
 
-    public setName(name: string): void {
-        this.name = name;
-    }
-    // Add more methods for managing slots if needed
 }
 
-export default Slot
+export default ParkingLot
