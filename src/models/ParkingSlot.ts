a6c061a32854949488b1fa9380380dc5ee364780
Luu Tuan Hoang <hoanglt0104@gmail.com>
2024-08-04 01:20:31 +0700

    Update ParkingSlot.ts

--- src/models/ParkingSlot.ts
+++ src/models/ParkingSlot.ts
@@ -3,13 +3,14 @@ import { SlotType, SlotStatus } from '@prisma/client';
 
 
 class ParkingSlot {
-    public id: number;
+    public id?: number;
     public type: SlotType;
     public status: SlotStatus;
 
-    constructor(type: SlotType, status: SlotStatus) {
+    constructor(type: SlotType, status: SlotStatus, id? : number) {
         this.type = type;
         this.status = status;
+        this.id = id;
     }
 }
 
